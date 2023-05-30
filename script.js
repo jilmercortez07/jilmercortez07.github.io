5b2bfc896a1722e25f3b3f9d04efe292869b2a49
Jilmer Cortez <jilmer.cortez07@gmail.com>
2023-05-30 16:58:39 -0500

    someUpdates

--- script.js
+++ script.js
@@ -236,74 +236,54 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 
+document.addEventListener('DOMContentLoaded', function() {
+  const user = document.getElementById('FrontEndBox');
+ 
+  const frontendProject = {
+    project1: {
+      nombre: 'Random Country Generator',
+      date: 'May 16, 2023',
+      image: "./images/random.png",
+      link: "./CountryGenerator/index.html"
+    },
+    project2: {
+      nombre: 'Bitcoin Halve Countdown',
+      date: 'May 16, 2023',
+      image: "./images/Bitcoin.png",
+      link:  "./BitcoinHalveCountdown/index.html"
+    },
+   
+  };
 
+  for (let key in frontendProject) {
+    const boxWrapper = document.createElement('div');
+    boxWrapper.classList.add('box-wrapper');
+    user.appendChild(boxWrapper);
 
-  document.addEventListener('DOMContentLoaded', function() {
-    const FrontEndContainers = document.getElementById('FrontEndBox');
-    const modalContainer = document.getElementById('modalContainer');
-    const modalImage = document.getElementById('modalImage');
-  
-    const names = {
-      person1: {
-        nombre: 'Random Country Generator',
-        date: 'May 16, 2023',
-        image: "./images/random.png",
-        websiteLink : "https://github.com/jilmercortez07/jilmercortez07.github.io",
-      },
-      person2: {
-        nombre: 'pepe',
-        property: 'ok',
-        image: "./images/Bitcoin.png",
-      },
-      
-      
-  
-    };
-  
-    for (let key in names) {
-      const boxWrapper = document.createElement('div');
-      boxWrapper.classList.add('box-wrapper');
-      FrontEndContainers.appendChild(boxWrapper);
-  
-      const squareThree = document.createElement('div');
-      squareThree.classList.add('squareThree');
-      boxWrapper.appendChild(squareThree);
-  
-      const nameLabel = document.createElement('div');
-      nameLabel.classList.add('labelThree');
-      nameLabel.textContent =  names[key].nombre;
-      boxWrapper.appendChild(nameLabel);
-  
-      const dateLabel = document.createElement('div');
-      dateLabel.classList.add('labelThree');
-      dateLabel.textContent = 'Date: ' + names[key].date;
-      boxWrapper.appendChild(dateLabel);
+    const square = document.createElement('div');
+    square.classList.add('squareFrontEnd');
+    boxWrapper.appendChild(square);
 
-      const websiteLink = document.createElement('a')
-      websiteLink.classList.add('labelThree');
-      websiteLink.href = names[key].websiteLink;
-      websiteLink.textContent = 'Github: ' + names[key].websiteLink;
-      websiteLink.target = '_blank';
-      boxWrapper.appendChild(websiteLink)
-  
-      const image = document.createElement('img');
-      image.src = names[key].image;
-      image.classList.add('square-image');
-      squareThree.appendChild(image);
-  
-      // Add click event listener to each image
-      image.addEventListener('click', function() {
-        modalImage.src = names[key].image;
-        modalContainer.classList.toggle('modal-open');
-      });
-    }
-  
-    // Close the modal when clicking outside the image
-    modalContainer.addEventListener('click', function(event) {
-      if (event.target === modalContainer) {
-        modalContainer.classList.remove('modal-open');
-      }
-    });
-  });
+    const nameLabel = document.createElement('div');
+    nameLabel.classList.add('labelFrontEnd');
+    nameLabel.textContent =  frontendProject[key].nombre;
+    boxWrapper.appendChild(nameLabel);
 
-  
+    const dateLabel = document.createElement('div');
+    dateLabel.classList.add('labelFrontEnd');
+    dateLabel.textContent = frontendProject[key].date;
+    boxWrapper.appendChild(dateLabel);
+
+    const imageLink = document.createElement('a');
+    imageLink.href = frontendProject[key].link;
+    imageLink.target = "_blank";
+
+    const image = document.createElement('img');
+    image.src = frontendProject[key].image;
+    image.classList.add('square-image');
+    square.appendChild(image);
+
+    imageLink.appendChild(image);
+    square.appendChild(imageLink);
+  } 
+});
